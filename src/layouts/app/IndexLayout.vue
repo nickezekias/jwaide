a39709b5b4df9d1324ea57056773a70458a95049
Nick Ezekiel <nickezekias@gmail.com>
2026-01-08 17:17:49 +0100

    fix layout and rm unused components

---
+++ src/layouts/app/IndexLayout.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import TopBar from '@/layouts/app/TopBar.vue'
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col">
+    <!-- <NikkToast /> -->
+
+    <TopBar />
+
+    <div class="flex flex-grow">
+      <main class="flex-grow max-w-screen pb-[100vh] md:pb-32 overflow-x-hidden">
+        <router-view />
+      </main>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
