3fdf54d5fcd08e625cc08c21c8eff0178598ddc4
Nick Ezekiel <nickezekias@gmail.com>
2026-01-08 16:32:12 +0100

    added i18n

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,20 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import PrimeVue from 'primevue/config'
-import primevueConfig from './lib/primevue'
-
 import App from './App.vue'
 import router from './router'
+
+import i18n from './locales/i18n'
+
+import PrimeVue from 'primevue/config'
+import primevueConfig from './lib/primevue'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(i18n)
 
 app.use(PrimeVue, primevueConfig)
 
